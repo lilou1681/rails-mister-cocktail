efe8e58eae1dc1d695588a5fc15bebad266e167a
Helene Petitjean <leloo8@msn.com>
2019-11-25 10:32:19 +0100

    mister cocktail

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,4 @@
 class Ingredient < ApplicationRecord
   has_many :doses
-  belongs_to :cocktail
   validates :name, uniqueness: true, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
 
-Cocktail.create(name: "Sex on the beach")
-Cocktail.create(name: "Pinacolada")
-Cocktail.create(name: "Margharita")
-Cocktail.create(name: "Whisky Coca")
+require 'open-uri'
+require 'json'
+puts 'Cleaning data base...'
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts "Create ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
